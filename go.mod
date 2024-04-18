0747cbf8b33fe4d470d94261f7038ca6f7187ad4
axodevelopment <71254585+axodevelopment@users.noreply.github.com>
2024-04-17 22:17:38 -0400

    Module base scaffolding ish

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/axodevelopment/servicebase
+
+go 1.22.0
---
+++ src/service.go
@@ -0,0 +1,19 @@
+package servicebase
+
+import (
+	"fmt"
+)
+
+type Service struct {
+	Name string `json:"Name"`
+}
+
+func New(name string) (*Service, error) {
+	fmt.Println("New called")
+
+	svc := &Service{
+		Name: name,
+	}
+
+	return svc, nil
+}
